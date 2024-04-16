e86a4d7042d5e387d881132af285de7695249410
DenisRudiy <denryd2311@gmail.com>
2024-04-16 18:34:28 +0300

    change carousel

--- src/components/Cases_components/Cases_slider.tsx
+++ src/components/Cases_components/Cases_slider.tsx
@@ -1,63 +1,43 @@
 import "../../styles/CasesCarousel.scss";
 import "../../styles/Adaptation.scss";
-import { useEffect, useRef, useState } from "react";
-import { activateSlide, getSlideIndex, nextSlide, prevSlide } from "../../services/CarouselService";
-import { handleSlide } from "../../services/titleAnimationService";
 import { carouselData } from "../../services/CarouselDataService";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 const CasesSlider = () => {
-  const titleRef = useRef<HTMLHeadingElement | null>(null);
-  let listRef = useRef<HTMLUListElement | null>(null);
-  const [activeIndex, setActiveIndex] = useState(6);
-
-  const handleItemClick = (index: any) => {
-    setActiveIndex(index);
-  };
-
-  const handleSlideClick = (e: any) => {
-    const max = window.matchMedia("screen and (max-width: 600px)").matches ? 5 : 8;
-
-    const slide = e.target.closest(".carousel__item") as any;
-    const index = slide ? getSlideIndex(slide) : -1;
-    if (index < 3 || index > max) return;
-    if (index === max) nextSlide(listRef.current);
-    if (index === 3) prevSlide(listRef.current);
-
-    activateSlide(slide);
-  };
-
-  useEffect(() => {
-    const handleScrollEvent = () => handleSlide(titleRef);
-    window.addEventListener("scroll", handleScrollEvent);
-    return () => {
-      window.removeEventListener("scroll", handleScrollEvent);
-    };
-  }, []);
-
   return (
-    <div className="CasesSlider">
-      <h1 className="CasesSlider_title" ref={titleRef}>
-        Our Works
-      </h1>
-      <div className="carousel">
-        <ul className="carousel__list" onClick={handleSlideClick} ref={listRef}>
-          {carouselData.map((item, index) => (
-            <li
-              className="carousel__item"
-              tabIndex={index}
-              key={index}
-              data-active={activeIndex === index ? "data-active" : null}
-              onClick={() => handleItemClick(index)}
-            >
-              <div className="carousel__box">
-                <div className="carousel__image">
-                  <img src={item.image} width="480" height="720" alt="" loading="lazy" />
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="carousel">
+      <Swiper
+        effect={"coverflow"}
+        spaceBetween={10}
+        slidesPerView={2}
+        loop={true}
+        grabCursor={true}
+        centeredSlides={true}
+        speed={300}
+        freeMode={false}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 50,
+          depth: 150,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        modules={[EffectCoverflow]}
+      >
+        {carouselData.map((item, index) => (
+          <SwiperSlide
+            className="carousel_item"
+            style={{ backgroundImage: `url(${item.image})` }}
+            key={index}
+          ></SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
